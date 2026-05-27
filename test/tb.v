9fa68e33b24a7d378169a4891a6b8bb70abe72e2
nev-1910 <nevinphilip10@gmail.com>
2026-05-27 18:36:04 +0530

    Updated UART transmitter testbench

--- test/tb.v
+++ test/tb.v
@@ -1,49 +1,61 @@
-`default_nettype none
 `timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module tb ();
+module tb;
 
-  // Dump the signals to a FST file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.fst");
-    $dumpvars(0, tb);
-    #1;
-  end
+    reg clk;
+    reg rst_n;
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
+    reg [7:0] ui_in;
 
-  // Replace tt_um_example with your module name:
-  tt_um_uart_tx dut (
+    wire [7:0] uo_out;
 
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
+    reg [7:0] uio_in;
+
+    reg ena;
+
+    // Instantiate DUT
+    tt_um_uart_tx dut (
+        .ui_in(ui_in),
+        .uo_out(uo_out),
+
+        .uio_in(uio_in),
+        .uio_out(uio_out),
+        .uio_oe(uio_oe),
+
+        .ena(ena),
+        .clk(clk),
+        .rst_n(rst_n)
+    );
+
+    // Clock generation
+    always #5 clk = ~clk;
+
+    initial begin
+
+        // Dump waveform
+        $dumpfile("tb.vcd");
+        $dumpvars(0, tb);
+
+        // Initialize signals
+        clk = 0;
+        rst_n = 0;
+        ena = 1;
+
+        ui_in = 8'b10110011;
+
+        uio_in = 8'b0;
+
+        // Apply reset
+        #20;
+        rst_n = 1;
+
+        // Run simulation
+        #300;
+
+        $finish;
+    end
 
 endmodule
